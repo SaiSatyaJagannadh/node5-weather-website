b924f14d1541332ba7d739a7dba47c9f80f7cd25
sai satya jagannadh doddipatla <saijagannadh1999@gmail.com>
2020-07-23 20:37:23 +0530

    added text

--- public/js/app.js
+++ public/js/app.js
@@ -1,56 +1,16 @@
-
-// console.log('we created javascript part')
-
-// const jsdom = require("jsdom");
-// const { JSDOM } = jsdom;
-
-// const fetch = require("node-fetch");
-
-
-
-// fetch('/weather?address=!').then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//             console.log(data.error)
-//         } else {
-//             console.log(data.location)
-//             console.log(data.longitude)
-
-
-//             // console.log(data.forecast)
-//         }
-//     })
-// })
-
-// const weatherform=document.querySelector('form')
-// const search=document.querySelector('input')
-
-// weatherform.addEventListener('submit',(e)=>
-// {
-//     e.preventDefault()
-//     const location=search.value
-//     console.log(location)
-// })
-
-
-
-
-
-
-
 console.log('Client side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
+const messageOne = document.querySelector("#message-1")
 const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
-
-    messageOne.textContent = 'Loading...'
+// console.log(location) 
+    messageOne.textContent = 'Loading...'//for showing
     messageTwo.textContent = ''
 
     fetch('/weather?address=' + location).then((response) => {
@@ -60,6 +20,7 @@ weatherForm.addEventListener('submit', (e) => {
             } else {
                 messageOne.textContent = data.location
                 // messageTwo.textContent = data.forecast
+
             }
         })
     })
